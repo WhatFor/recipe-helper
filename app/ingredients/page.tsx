68d36a4e34fff1a5ec7f7ff15acfb558e3094bc0
Chris Walker <chris@awsm.tech>
2024-10-23 19:33:19 +0100

    WIP

--- app/ingredients/page.tsx
+++ app/ingredients/page.tsx
@@ -1,14 +1,22 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { ingredientsTable } from "@/db/schema";
+import Header from "@/components/ui/header";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const IngredientsPage = async () => {
   const db = drizzle();
   const result = await db.select().from(ingredientsTable);
 
   return (
-    <div>
-      <h1>Ingredients</h1>
-      <a href="/ingredients/new">New Ingredient</a>
+    <div className="flex flex-col gap-y-8">
+      <Header>Ingredients</Header>
+      <Button asChild>
+        <Link href="/ingredients/new">New Ingredient</Link>
+      </Button>
+      <Button asChild>
+        <Link href="/">Home</Link>
+      </Button>
       <ul>
         {result.map((ingredient) => (
           <li key={ingredient.id}>{ingredient.name}</li>
