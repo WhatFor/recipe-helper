5a9a970d00180adcf953e2d3ce874b3aad01f22b
Chris Walker <chris@awsm.tech>
2024-10-29 20:57:38 +0000

    Remove unused NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,33 +10,6 @@ export const metadata: Metadata = {
   description: "Track recipes",
 };
 
-// const NavItem = ({
-//   href,
-//   children,
-// }: {
-//   href: string;
-//   children: React.ReactNode;
-// }) => {
-//   return (
-//     <a
-//       className="px-3 py-1 w-1/3 border rounded-md bg-foreground/20 hover:bg-foreground/30 transition-all"
-//       href={href}
-//     >
-//       {children}
-//     </a>
-//   );
-// };
-
-const NavBar = () => {
-  return (
-    <menu className="flex p-2 gap-x-3 w-full justify-stretch">
-      {/* <NavItem href="/ingredients">Ingredients</NavItem>
-      <NavItem href="/recipes">Recipes</NavItem>
-      <NavItem href="/blocks">Blocks</NavItem> */}
-    </menu>
-  );
-};
-
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -49,7 +22,6 @@ export default async function RootLayout({
           <NextTopLoader color="#fff" />
           <RequireAuth>
             <div className="flex flex-col justify-center items-center h-full">
-              <NavBar />
               <div className="w-full h-full max-w-7xl flex flex-col gap-y-12 pb-8 pt-4 px-4">
                 {children}
               </div>
